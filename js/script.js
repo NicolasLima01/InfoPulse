3862a6f558ea22ff3de3ea1606c6cd1a3533f63d
NicolasLima01 <nicolasrlima@protonmail.com>
2026-07-07 15:21:00 -0300

    Documentando arquivos CSS, JS e HTML
    Comentando código para revisões e manutenções futuras. Adicionando focus no input de pesquisa ao clicar no btn-open-search

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-// NAVBAR
+// ----------------------------------------
+//                LOGO NAVBAR
+// ----------------------------------------
 
 let navBar = document.getElementById('navbar'); //declarando variável que recebe navbar
 let LogoNavBar = document.getElementById('logo-navbar'); //declarando variável que recebe Logo no navbar
@@ -20,6 +22,10 @@ window.addEventListener("scroll", () => {
 })
 
 
+// ----------------------------------------
+//             PESQUISA NAVBAR
+// ----------------------------------------
+
 let btnAbrirPesquisa = document.getElementById("btn-nav-open-search"); // variável que recebe elemento do botão que abre o form depesquisa do navbar
 let FormPesquisa = document.getElementById("form-nav-search"); // variável que recebe elemento do botão de pesquisa do navbar
 let btnSubmeterPesquisa = document.getElementById("btn-nav-submit-search"); // variável que recebe elemento do botão que conclui pesquisa do navbar
@@ -27,11 +33,19 @@ let btnSubmeterPesquisa = document.getElementById("btn-nav-submit-search"); // v
 // evento que ocorre ao clicar no botão de pesquisa
 btnAbrirPesquisa.addEventListener('click', function () {
 
-    // if ternário que ocorre se o FormPesquisa estiver hidden, tornará o form visível,
-    // mas se o FormPesquisa estiver à visível, tornará o form visível
-    FormPesquisa.hidden === true ? FormPesquisa.hidden = false : FormPesquisa.hidden = true;
+    // Ocorre se o FormPesquisa estiver escondido
+    if (FormPesquisa.hidden === true) {
+        FormPesquisa.hidden = false //Faz o form ficar visível
+        FormPesquisa.focus(); //Faz o input de pesquisa ficar em foco
+    }
 
-    //if ternário semelhante ao de cima, porém, para o botão de submeter pesquisa
+    // Ocorre se FormPesquisa estiver  estiver à mostra
+    else {
+        FormPesquisa.hidden = true; //Faz o form ficar escondido
+    }
+
+    // if ternário, se o btn de submit estiver escondido, o btn ficará visível,
+    // senão, o btn ficará escondido
     btnSubmeterPesquisa.hidden === true ? btnSubmeterPesquisa.hidden = false : btnSubmeterPesquisa.hidden = true;
 });
 
